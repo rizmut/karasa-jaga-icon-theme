54079ac0f3bb7413e731e3f72db144cd758e5a94
Rizal Muttaqin <riz_17_oke@yahoo.co.id>
2020-04-16 12:41:40 +0700

    Update SVG canvas size, import icon from LibreOffice version

---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#2020 by Rizal Muttaqin
+
+if ! command -v optipng >/dev/null
+then
+    echo "Please install optipng"
+    exit 1
+fi
+
+if ! command -v svgcleaner >/dev/null
+then
+    echo "Please install svgcleaner"
+    exit 1
+fi
+
+rm -Rf "build/Karasa-Jaga/*"
+
+cd "src/Karasa-Jaga"
+
+echo "=> Clean SVG files ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do 
+	echo "This $i file is compressed"
+	fname=$( basename "$i")
+#	echo "has the name: $fname"
+	fdir=$( dirname "$i")
+#	echo "and is in the directory: ${fdir##*/}"
+	svgcleaner "$i" "${i%.*}.svg"
+done
+
+cd "./../.."
+cp -Rf "src/Karasa-Jaga" \
+   "build/Karasa-Jaga"
+cd "build/Karasa-Jaga"
+
+echo "=> Export SVG to PNG ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do 
+	echo "This $i file is compressed"
+	fname=$( basename "$i")
+	fdir=$( dirname "$i")
+	inkscape -f "$i" -e "${i%.*}.png"
+	optipng -o7 "${i%.*}.png"
+done
+
+echo "=> Delete SVG files ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do
+    fname=$( basename "$i")
+    fdir=$( dirname "$i")
+    rm "$i"
+done
